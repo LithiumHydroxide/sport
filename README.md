35f8e0ce5663906f339755cb45cf1df96a735cd7
LithiumHydroxide <94555306+LithiumHydroxide@users.noreply.github.com>
2024-06-12 13:39:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Match-booking ticketing system
+ This is a ticketing system that books matches and which seat is available
---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$title = 'Online Ticket Reservation System';
+//E-Ticketing System For Football Matches
+$supervisor_name = "Anthony Kang'au ";
+$developer_name = "Rebecca Adeng Adeng";
+$developer_matric = "16/52HA016";
